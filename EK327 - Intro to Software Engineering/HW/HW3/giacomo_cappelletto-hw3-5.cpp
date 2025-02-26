8640f23ef1c9f152532b7668a00bcca9e5b7ad1f
JJCAPPE <giacomo.cappelletto@icloud.com>
2025-02-26 14:34:51 -0500

    done hw3

--- EK327 - Intro to Software Engineering/HW/HW3/giacomo_cappelletto-hw3-5.cpp
+++ EK327 - Intro to Software Engineering/HW/HW3/giacomo_cappelletto-hw3-5.cpp
@@ -1,6 +1,6 @@
 #include "hw3_problem5.h"
 #include <stdlib.h>
-
+#include <stdio.h>
 /*
 25 -> 10 10 5
 10 -> 5 5
@@ -19,24 +19,44 @@ mod 25 -> remainder
 Can do this recursively
 */
 
-CoinArray returnComposition(int remainder, Coin divisor){
-    CoinArray composition;
-    int currentRemainder = remainder / divisor;
-    if (currentRemainder > 0){
-        
+
+#define NUM_COINS 4
+Coin availableCoins[NUM_COINS] = {PENNY, NICKEL, DIME, QUARTER};
+
+void findCombinations(int amount, int start, Coin *current, int currentSize, CoinArrays *result) {
+    if (amount == 0) {
+        Coin *combo = (Coin*)malloc(currentSize * sizeof(Coin));
+        for (int i = 0; i < currentSize; i++) {
+            combo[i] = current[i];
+        }
+        result->arrays = (CoinArray*)realloc(result->arrays, (result->size + 1) * sizeof(struct CoinArray));
+        result->arrays[result->size].coins = combo;
+        result->arrays[result->size].size = currentSize;
+        result->size++;
+        return;
+    }
+    for (int i = start; i < NUM_COINS; i++) {
+        if (availableCoins[i] > amount)
+            break; // Since coins are in ascending order, no coin will fit.
+        current[currentSize] = availableCoins[i];
+        findCombinations(amount - availableCoins[i], i, current, currentSize + 1, result);
     }
 }
 
-CoinArrays possibleChangeAmounts(int totalCents){
-    CoinArrays coinArrays;
 
-
-
-    return coinArrays;
+CoinArrays possibleChangeAmounts(int totalCents) {
+    CoinArrays result;
+    result.arrays = NULL;
+    result.size = 0;
+    Coin tempCombination[totalCents];  
+    findCombinations(totalCents, 0, tempCombination, 0, &result);
+    qsort(result.arrays, result.size, sizeof(struct CoinArray), compareCoinArrays);
+    return result;
 }
 
-int main(){
-    int result = 0 / 25;
-    printf("%d\n", result);
+int main() {
+    int total = 31;
+    CoinArrays allCombinations = possibleChangeAmounts(total);
+    printCoinArrays(allCombinations);
     return 0;
 }
