c59198d7e877f34c4bc94d2ec37409c316b61589
JJCAPPE <giacomo.cappelletto@icloud.com>
2025-03-27 11:09:28 -0400

    finished lab4a, fixed hw4

--- EK327 - Intro to Software Engineering/HW/HW4/GaussianInteger.cpp
+++ EK327 - Intro to Software Engineering/HW/HW4/GaussianInteger.cpp
@@ -1,74 +1,70 @@
 #include "GaussianInteger.h"
+#include <stdio.h>
 
-GaussianInteger::GaussianInteger() : ComplexNumber(0, 0){}
+GaussianInteger::GaussianInteger() : ComplexNumber(0, 0) {}
 GaussianInteger::GaussianInteger(const int real, const int imag) : ComplexNumber(real, imag) {}
 GaussianInteger::GaussianInteger(const GaussianInteger &other) : ComplexNumber(other.getReal(), other.getImag()) {}
 
-ComplexNumber* GaussianInteger::add(const ComplexNumber &other) {
+ComplexNumber *GaussianInteger::add(const ComplexNumber &other)
+{
     return new GaussianInteger(
-        (other.getReal() + this->getReal()), 
-        (other.getImag() + this->getImag())
-    );
+        (other.getReal() + this->getReal()),
+        (other.getImag() + this->getImag()));
 }
-ComplexNumber* GaussianInteger::multiply(const ComplexNumber &other) {
+ComplexNumber *GaussianInteger::multiply(const ComplexNumber &other)
+{
     int a = other.getReal();
     int b = other.getImag();
     int c = this->getReal();
     int d = this->getImag();
     return new GaussianInteger(
-        a * c - b * d, a * d + b * c
-    );
+        a * c - b * d, a * d + b * c);
 }
-ComplexNumber* GaussianInteger::conjugate() {
+ComplexNumber *GaussianInteger::conjugate()
+{
     return new GaussianInteger(
-        this->getReal(), 
-        (-1)*this->getImag()
-    );
+        this->getReal(),
+        (-1) * this->getImag());
 }
-int GaussianInteger::norm(){
+int GaussianInteger::norm()
+{
     return (this->getReal() * this->getReal()) + (this->getImag() * this->getImag());
 }
 
-/**
- * TEST(DivisionTest, WildcardDivisionTest) {
-    GaussianInteger* dividend = new GaussianInteger(-12, 72);
-    GaussianInteger* divisor = new GaussianInteger(6, 1);
-    bool result = dividend->divides(*divisor);
-
-    EXPECT_TRUE(result);
-}
- */
-bool GaussianInteger::divides(const GaussianInteger &other) {
-    GaussianInteger nonConstOther(other);
-    int normThis = this->norm();
-    if (normThis == 0) {
-        return false; 
+bool GaussianInteger::divides(const GaussianInteger &other)
+{
+    // 'other' is the divisor: a + bi.
+    int a = other.getReal();
+    int b = other.getImag();
+    int normDivisor = a * a + b * b;
+    if (normDivisor == 0)
+    {
+        return false; // division by zero not allowed.
     }
-    
-    std::unique_ptr<ComplexNumber> conjThis(this->conjugate());
-    
-    std::unique_ptr<ComplexNumber> numerator(nonConstOther.multiply(*conjThis));
-    
-    int numReal = numerator->getReal();
-    int numImag = numerator->getImag();
-    
-    return (numReal % normThis == 0 && numImag % normThis == 0);
+
+    // 'this' is the dividend: c + di.
+    int c = this->getReal();
+    int d = this->getImag();
+
+    // Compute (a*c + b*d) / (a^2+b^2) and (a*d - b*c) / (a^2+b^2)
+    bool realQuotientInteger = ((a * c + b * d) % normDivisor == 0);
+    bool imagQuotientInteger = ((a * d - b * c) % normDivisor == 0);
+
+    return realQuotientInteger && imagQuotientInteger;
 }
 
 GaussianInteger GaussianInteger::operator+(const GaussianInteger &other) const
 {
     return GaussianInteger(
         this->getReal() + other.getReal(),
-        this->getImag() + other.getImag()
-    );
+        this->getImag() + other.getImag());
 }
 
 GaussianInteger GaussianInteger::operator*(const GaussianInteger &other) const
 {
     return GaussianInteger(
         this->getReal() * other.getReal() - this->getImag() * other.getImag(),
-        this->getReal() * other.getImag() + this->getImag() * other.getReal()
-    );
+        this->getReal() * other.getImag() + this->getImag() * other.getReal());
 }
 
 bool GaussianInteger::operator==(const GaussianInteger &other) const
@@ -81,3 +77,12 @@ std::ostream &operator<<(std::ostream &os, const GaussianInteger &other)
     os << other.getReal() << (other.getImag() >= 0 ? " + " : " - ") << std::abs(other.getImag()) << "i";
     return os;
 }
+
+/* int main()
+{
+    GaussianInteger *dividend = new GaussianInteger(-12, 72);
+    GaussianInteger *divisor = new GaussianInteger(6, 1);
+    bool result = dividend->divides(*divisor);
+    printf("%s\n", (result == true) ? "true" : "false"); // should be true
+    return 0;
+} */
